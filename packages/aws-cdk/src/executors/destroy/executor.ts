0bf1ab13edd340a0d25d8f3a744ed1fe2869267a
StellarScript <ej92@protonmail.com>
2023-11-14 21:47:57 -0800

    aws-cdk executors setup

---
+++ packages/aws-cdk/src/executors/destroy/executor.ts
@@ -0,0 +1,8 @@
+import { DestroyExecutorSchema } from './schema';
+
+export default async function runExecutor(options: DestroyExecutorSchema) {
+  console.log('Executor ran for Destroy', options);
+  return {
+    success: true,
+  };
+}
