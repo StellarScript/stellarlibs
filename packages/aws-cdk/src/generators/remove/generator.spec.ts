3783cecc1c0455333c1e70f8dbfb4e898a9f3997
StellarScript <ej92@protonmail.com>
2023-11-14 21:10:26 -0800

    plugin generators init

---
+++ packages/aws-cdk/src/generators/remove/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nx/devkit';
+
+import { removeGenerator } from './generator';
+import { RemoveGeneratorSchema } from './schema';
+
+describe('remove generator', () => {
+  let tree: Tree;
+  const options: RemoveGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    tree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should run successfully', async () => {
+    await removeGenerator(tree, options);
+    const config = readProjectConfiguration(tree, 'test');
+    expect(config).toBeDefined();
+  });
+});
