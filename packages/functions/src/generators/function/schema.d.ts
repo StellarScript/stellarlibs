2e8c1176dc90d3a4e494e8f5a5dd4589796f62b4
StellarScript <ej92@protonmail.com>
2024-01-31 02:07:32 -0800

    update

--- packages/functions/src/generators/function/schema.d.ts
+++
@@ -1,4 +0,0 @@
-export interface FunctionGeneratorSchema {
-  name: string;
-  project: string;
-}
--- packages/functions/src/generators/lib/schema.d.ts
+++
@@ -1,5 +0,0 @@
-import { GeneratorAppSchema } from '../../shared/generator/schema';
-
-export interface LibGeneratorSchema extends GeneratorAppSchema {
-  name: string;
-}
