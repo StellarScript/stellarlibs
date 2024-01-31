d384d3ddee78bfa09196f17ce3258046f507edc8
StellarScript <ej92@protonmail.com>
2024-01-31 11:15:11 -0800

    update

--- packages/testapp/src/bin/index.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-
-import { App } from 'aws-cdk-lib';
-import { AppStack } from '../stack/app';
-
-const app = new App();
-new AppStack(app, 'testapp');
