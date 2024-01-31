916a32b08bc3a55dbaf501d1a5e09779084cebd8
StellarScript <ej92@protonmail.com>
2024-01-31 08:13:39 -0800

    update packages

--- packages/nx-cdk-e2e/jest.config.ts
+++ packages/nx-cdk-e2e/jest.config.ts
@@ -6,7 +6,7 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/e2e/nx-cdk-e2e',
+  coverageDirectory: '../../coverage/packages/nx-cdk-e2e',
   globalSetup: '../../tools/scripts/start-local-registry.ts',
   globalTeardown: '../../tools/scripts/stop-local-registry.ts',
 };
