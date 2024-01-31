c4d2eda0a9145c0a86159c726a89fe04d8bf8852
StellarScript <ej92@protonmail.com>
2024-01-31 03:45:50 -0800

    app generator unit test

--- packages/nx-cdk/src/generators/app/generator.spec.ts
+++ packages/nx-cdk/src/generators/app/generator.spec.ts
@@ -1,20 +1,31 @@
 import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
 import { Tree, readProjectConfiguration } from '@nx/devkit';
 
-import { appGenerator } from './generator';
+import applicationGenerator from './generator';
 import { AppGeneratorSchema } from './schema';
 
-describe('app generator', () => {
+describe('Application Generator', () => {
   let tree: Tree;
-  const options: AppGeneratorSchema = { name: 'test' };
+  const options: AppGeneratorSchema = { name: 'testapp' };
 
   beforeEach(() => {
     tree = createTreeWithEmptyWorkspace();
   });
 
-  it('should run successfully', async () => {
-    await appGenerator(tree, options);
-    const config = readProjectConfiguration(tree, 'test');
+  it('generate application', async () => {
+    await applicationGenerator(tree, options);
+    const config = await readProjectConfiguration(tree, options.name);
     expect(config).toBeDefined();
+    expect(tree.exists(config.sourceRoot)).toBeTruthy();
+  });
+
+  it('generate application with directory argument', async () => {
+    const directory = 'dir';
+    const _options = { ...options, directory };
+
+    await applicationGenerator(tree, _options);
+    const config = await readProjectConfiguration(tree, _options.name);
+    expect(config).toBeDefined();
+    expect(tree.exists(config.sourceRoot)).toBeTruthy();
   });
 });
