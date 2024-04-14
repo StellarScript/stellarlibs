0f735299557d563aa5c82565135789a44e5652d6
StellarScript <150498398+StellarScript@users.noreply.github.com>
2024-04-13 23:21:49 -0700

    dependencies

--- packages/nest-serverless/src/generators/app/dependencies.ts
+++ packages/nest-serverless/src/generators/app/dependencies.ts
@@ -1,9 +1,12 @@
 export const devDependencies = {
    serverless: '^3.38.0',
+   'aws-lambda': '^1.0.7',
    '@types/serverless': '^3.12.22',
    'serverless-esbuild': '^1.52.1',
    'serverless-localstack': '^1.2.0',
    'serverless-offline': '^13.3.3',
+   '@vendia/serverless-express': '^4.12.6',
+   '@nestjs/platform-express': '^10.0.2',
 };
 
 export const dependencies = {
