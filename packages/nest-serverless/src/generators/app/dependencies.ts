d9ac5058d1a2c21d71c35b9117ef281c65013222
StellarScript <ej92@protonmail.com>
2024-04-17 03:23:11 -0700

    webpack generator

--- packages/nest-serverless/src/generators/app/dependencies.ts
+++ packages/nest-serverless/src/generators/app/dependencies.ts
@@ -2,15 +2,17 @@ export const dependencies = {
    '@nestjs/common': '^10.0.2',
    '@nestjs/config': '^3.2.2',
    '@nestjs/core': '^10.0.2',
-   '@nestjs/passport': '^10.0.3',
    '@nestjs/platform-express': '^10.0.2',
    '@vendia/serverless-express': '^4.12.6',
    'reflect-metadata': '^0.2.2',
+   'class-transformer': '^0.5.1',
+   'class-validator': '^0.14.1',
 };
 
 export const devDependencies = {
-   esbuild: '^0.19.2',
-   '@nx/esbuild': '18.0.4',
+   '@nx/webpack': '^18.2.3',
+   webpack: '^5.91.0',
+   'webpack-cli': '^5.1.4',
    'aws-lambda': '^1.0.7',
    '@types/aws-lambda': '^8.10.137',
    '@nx/nest': '18.0.4',
